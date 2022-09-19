a9e2dc2c46c81a24663167e4411ff5c06bca4211
Aleksandr-Alekseenko-23 <alekseenko48@bigmir.net>
2022-09-19 17:46:20 +0300

    fix toast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
-import { toast } from 'react-toastify';
 import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Button from './Button/Button';
